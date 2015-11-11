bfd7d69b8fd7ab7a8382adc4313bde3b687a66b4
huylv <levanhuy93@gmail.com>
2015-11-11 17:21:44 +0700

    - language

--- upload/admin/language/english/catalog/product.php
+++ upload/admin/language/english/catalog/product.php
@@ -26,6 +26,9 @@ $_['column_action']          = 'Action';
 
 // Entry
 $_['entry_name']             = 'Product Name';
+
+$_['entry_test']             = 'TEST';
+
 $_['entry_description']      = 'Description';
 $_['entry_meta_title'] 	     = 'Meta Tag Title';
 $_['entry_meta_keyword'] 	 = 'Meta Tag Keywords';
